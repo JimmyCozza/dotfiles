8b16eb943c1f3c9f8d30dc80c2fbd3a73c5115b1
Jimmy Cozza <moatcozza@gmail.com>
2021-06-02 08:04:07 -0600

    add firefox & bat to pacman

--- manjaroSetup.sh
+++ manjaroSetup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 ##################################################
+################# Programs #######################
+##################################################
+echo "Downloading plug/tmux/oh my zsh"
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+##################################################
 ################# Symlinks #######################
 ##################################################
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mv $HOME/.zshrc $HOME/.zshrc.bak
 echo "symlinking dotfiles"
 ln -s "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
+
 source "$HOME/.zshrc"
+
 ln -s "$CONFIG_FILES_PATH/alacritty.yml" "$HOME/.alacritty.yml"
 ln -s "$CONFIG_FILES_PATH/tmux.conf" "$HOME/.tmux.conf"
 ln -s "$CONFIG_FILES_PATH/nvim" "$HOME/.config/nvim"
@@ -18,9 +26,8 @@ ln -s "$CONFIG_FILES_PATH/gitignore_global" "$HOME/.gitignore_global"
 ################## Pacman ########################
 ##################################################
 
-sudo pacman -Syu --noconfirm alacritty fzf ripgrep tmux
+sudo pacman -Syuv --noconfirm alacritty fzf ripgrep tmux firefox bat
 source "$HOME/.zshrc"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ##################################################
 ############### AUR Packages #####################
@@ -32,7 +39,6 @@ pamac install neovim-nightly-bin fnm-bin lazygit-bin lazydocker nerd-fonts-go-mo
 ################## Neovim ########################
 ##################################################
 
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 source "$HOME/.zshrc"
 sudo pip3 install neovim
 sudo pip3 install neovim-remote
