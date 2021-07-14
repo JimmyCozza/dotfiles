f9c0608604d53b9b607f33ce041a86b0c9011eb2
Jimmy Cozza <moatcozza@gmail.com>
2021-07-14 16:04:20 -0600

    add lazygit command

--- nvim/lua/configs/floatterm.lua
+++ nvim/lua/configs/floatterm.lua
@@ -7,3 +7,22 @@ require('FTerm').setup({
     },
     border = 'single'
 })
+
+local term = require("FTerm.terminal")
+
+local gitui = term:new():setup({
+    cmd = "lazygit",
+    dimensions = {
+      height = 0.85,
+      width = 0.85,
+      x = 0.5,
+      y = 0.5
+    }
+})
+
+ -- Use this to toggle gitui in a floating terminal
+function _G.__fterm_gitui()
+    gitui:toggle()
+end
+  
+
