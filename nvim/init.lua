7e4865f7be0fd35e6b0fe95968e388022035e397
Jimmy Cozza <moatcozza@gmail.com>
2021-07-14 09:15:20 -0600

    All my old packages & general settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,37 @@
 require("mappings")
 require("plugins")
+require("configs.which-key")
+
+-- ****************************
+-- General Settings
+-- ****************************
+vim.g.mapleader      = ' '
+vim.opt.number       = true
+vim.opt.wrap         = false
+vim.opt.tabstop      = 2
+vim.opt.softtabstop  = 2
+vim.opt.shiftwidth   = 2
+vim.opt.hidden       = true
+vim.opt.expandtab    = true
+vim.opt.ignorecase   = true
+vim.opt.smartcase    = true
+vim.opt.clipboard    = 'unnamedplus'
+vim.opt.cursorline   = true
+vim.opt.inccommand   = 'nosplit'
+vim.opt.mouse        = 'a'
+vim.opt.swapfile     = false
+vim.opt.undofile     = true
+-- vim.opt.undodir      = $HOME/.vim/tmp/undo/
+vim.opt.completeopt  = 'menu,preview'
+vim.opt.splitbelow   = true
+vim.opt.splitright   = true
+
+
+-- ****************************
+-- Visual Settings
+-- ****************************
+vim.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = true
+require('nord').set()
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,6 @@
+-- ****************************
+-- Bootstrapping Packer
+-- ****************************
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
@@ -7,3 +10,81 @@ if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
 end
+
+
+-- ****************************
+-- Bootstrapping Packer
+-- ****************************
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use '9mm/vim-closer'
+  use 'junegunn/seoul256.vim'
+  use 'junegunn/limelight.vim'
+  use 'ryanoasis/vim-devicons'
+  use 'eddyekofo94/gruvbox-flat.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'folke/lsp-colors.nvim'
+  use 'hoob3rt/lualine.nvim'
+  use 'shaunsingh/nord.nvim'
+  use 'ap/vim-css-color'
+  
+  --Navigation
+  use 'kyazdani42/nvim-tree.lua'
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  --use 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
+  
+  --Father Pope
+  use 'tpope/vim-abolish'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-dadbod'
+  
+  use 'kristijanhusak/vim-dadbod-ui'
+  
+  --Undo
+  use 'mbbill/undotree'
+  use 'simnalamburt/vim-mundo'
+  
+  --Git
+  use 'tpope/vim-fugitive'
+  use 'junegunn/gv.vim' --Git commit browser. Needs fugitive
+  use 'AndrewRadev/linediff.vim' --Creates diff buffer between selected line(s)
+  
+  --Utilities
+  use 'vimwiki/vimwiki'
+  --use 'JamshedVesuna/vim-markdown-preview', { 'for': 'markdown' }
+  use 'scrooloose/nerdcommenter'
+  use 'jiangmiao/auto-pairs'
+  use 'junegunn/vim-easy-align'
+  use 'SirVer/ultisnips'
+  use 'folke/which-key.nvim'
+  use 'folke/trouble.nvim'
+  use 'svermeulen/vimpeccable'
+  use 'kristijanhusak/orgmode.nvim'
+  
+  --Javascript/Node
+  use 'pangloss/vim-javascript'
+  use 'mxw/vim-jsx'
+  use 'othree/yajs.vim'
+  use 'othree/javascript-libraries-syntax.vim'
+  use 'leafgarland/typescript-vim'
+  
+  --Other Language Stuff
+  use 'neovim/nvim-lspconfig'
+  use 'glepnir/lspsaga.nvim'
+  use 'hrsh7th/nvim-compe'
+  use 'dense-analysis/ale'
+  use {
+    'fatih/vim-go',
+    ft = '.go'
+  }
+  --use 'fatih/vim-go', { 'do': ':GoInstallBinaries' }
+  use 'rust-lang/rust.vim'
+  
+end)
+
