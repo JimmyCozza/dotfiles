b6a238790732dbf21880c9e4a811ea34954cbc83
Jimmy Cozza <moatcozza@gmail.com>
2021-07-15 00:02:14 -0600

    left

--- nvim/lua/configs/nvimtree.lua
+++ nvim/lua/configs/nvimtree.lua
@@ -1,4 +1,4 @@
-vim.g.nvim_tree_side = 'right'
+vim.g.nvim_tree_side = 'left'
 vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
