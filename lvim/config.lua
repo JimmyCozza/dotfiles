f589724f5ee41c626622bc59aeeeaf97ec215012
Jimmy Cozza <moatcozza@gmail.com>
2021-08-12 15:54:55 -0600

    comma drama

--- lvim/config.lua
+++ lvim/config.lua
@@ -184,7 +184,7 @@ lvim.plugins = {
         -- refer to the configuration section below
       }
     end
-  }
+  },
   {"ray-x/lsp_signature.nvim",
     config = function() require"lsp_signature".on_attach() end,
     event = "InsertEnter"
