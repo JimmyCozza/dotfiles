5c199b11b7ba21ea97e3f9b26371d725dfc71b27
Jimmy Cozza <moatcozza@gmail.com>
2021-09-17 13:57:07 -0600

    why not?

--- doom/config.el
+++ doom/config.el
@@ -130,3 +130,26 @@
   (set-face-attribute (car face) nil :font "GoMono Nerd Font" :weight 'regular :height (cdr face)))
   (setq org-ellipsis " ▾")
   (org-add-link-type "youtrack" #'make-youtrack-link))
+
+;; Loads ox-gfm for github flavored markdown exports
+(eval-after-load "org"
+  '(require 'ox-gfm nil t))
+
+
+;;===================================================================================
+;;                                    ORG ROAM UI
+;; ===================================================================================
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
