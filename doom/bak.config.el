00a491de25549b9deb51a1c258d37b94a8fee432
Jimmy Cozza <moatcozza@gmail.com>
2021-10-21 13:00:08 -0600

    get a couple things together

---
+++ doom/bak.config.el
@@ -0,0 +1,155 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Jimmy Cozza"
+      user-mail-address "")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "GoMono Nerd Font" :size 16)
+      doom-variable-pitch-font (font-spec :family "GoMono Nerd Font" :size 16))
+      ;; doom-variable-pitch-font (font-spec :family "Fira Code Nerd Font" :size 18))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'gruvbox-dark-soft)
+(setq doom-themes-treemacs-theme "doom-colors")
+
+;; Transparent background
+ (set-frame-parameter (selected-frame) 'alpha '(92 . 90))
+ (add-to-list 'default-frame-alist '(alpha . (92 . 90)))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; New Keybindings
+(map! :leader
+      :desc "nohlsearch"
+      "s c " #'evil-ex-nohighlight)
+
+(map! :map org-mode-map
+      :leader
+      :desc "Update All Dblocks"
+      "m u" #'org-update-all-dblocks)
+
+(map! :leader
+      :desc "Comment Line"
+      "c l " #'comment-line)
+
+;;===================================================================================
+;;                                    JAVASCRIPT
+;; ===================================================================================
+;; (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log"))
+(add-hook 'js2-mode-hook 'eslintd-fix-mode)
+(require 'dap-node)
+(setq dap-node-debug-program "~/tools/vscode-node-debug2/out/src/nodeDebug.js")
+;; (setq lsp-log-io t)
+(setq dap-print-io t)
+(dap-register-debug-template
+  "Attach to node process "
+  (list :type "node"
+        :request "attach"
+        :program "__ignored"
+        :port 9230
+        :name "Attach to node process in docker container"))
+ ;(defun you-track-integration ()
+  ;(interactive)
+  ;(insert (shell-command-to-string (format "python3 %s" buffer-file-name))))
+;;===================================================================================
+;;                                    ORG MODE
+;; ===================================================================================
+;; Setting a TODO to DONE sets a Closed timestamp
+(setq org-directory "~/notes/org/")
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+(setq org-agenda-start-with-log-mode t)
+
+(setq org-agenda-files
+'("~/notes/org/habit.org"
+  "~/notes/org/todo.org"
+  "~/notes/org/notes.org"
+  "~/notes/org/organizer.org"
+  "~/notes/org/projects.org"
+  "~/notes/org/schedule.org"
+  "~/notes/org/standups.org"))
+
+(defun make-youtrack-link (yt_id)
+  (browse-url (concat "https://growmies.myjetbrains.com/youtrack/issue/GA-" yt_id)))
+(after! org
+  ;; Org Habit
+  (require 'org-habit)
+  (add-to-list 'org-modules 'org-habit)
+  (setq org-habit-graph-column 60)
+  ;; Replace list hyphen with dot
+  (defun efs/org-font-setup ()
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.5)
+                  (org-level-2 . 1.3)
+                  (org-level-3 . 1.1)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 0.9)
+                  (org-level-6 . 0.9)
+                  (org-level-7 . 0.9)
+                  (org-level-8 . 0.9)))
+  (set-face-attribute (car face) nil :font "GoMono Nerd Font" :weight 'regular :height (cdr face)))
+  (setq org-ellipsis " ▾")
+  (org-add-link-type "youtrack" #'make-youtrack-link))
+
+;; Loads ox-gfm for github flavored markdown exports
+(eval-after-load "org"
+  '(require 'ox-gfm nil t))
+
+
+;;===================================================================================
+;;                                    ORG ROAM UI
+;; ===================================================================================
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
--- doom/config.el
+++ doom/config.el
@@ -1,11 +1,17 @@
+(defun efs/org-babel-tangle-config()
+  (when (string-equal (file-name-directory (buffer-file-name))
+                      (expand-file-name "~/dotfiles/doom/"))
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
 (setq doom-font (font-spec :family "GoMono Nerd Font" :size 16)
       doom-variable-pitch-font (font-spec :family "GoMono Nerd Font" :size 16))
 
 (setq doom-theme 'gruvbox-dark-soft)
 (setq doom-themes-treemacs-theme "doom-colors")
 
- (set-frame-parameter (selected-frame) 'alpha '(92 . 90))
- (add-to-list 'default-frame-alist '(alpha . (92 . 90)))
+(set-frame-parameter (selected-frame) 'alpha '(92 . 90))
+(add-to-list 'default-frame-alist '(alpha . (92 . 90)))
 
 (setq display-line-numbers-type t)
 
@@ -22,6 +28,9 @@
 (map! :leader
       :desc "Comment Line"
       "c l " #'comment-line)
+;; Left and Right move to linting errors
+(map! :n "<left>" #'flycheck-previous-error)
+(map! :n "<right>" #'flycheck-next-error)
 
 ;; (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log"))
 (add-hook 'js2-mode-hook 'eslintd-fix-mode)
@@ -84,59 +93,21 @@
 (eval-after-load "org"
   '(require 'ox-gfm nil t))
 
-(defun efs/exwm-update-class ()
-  (exwm-workspace-rename-buffer exwm-class-name))
+;; Each path is relative to the path of the maildir you passed to mu
+(set-email-account! "moatcozza@gmail.com"
+  '((mu4e-sent-folder       . "/gmail/Sent Mail")
+    (mu4e-drafts-folder     . "/gmail/Drafts")
+    (mu4e-trash-folder      . "/gmail/Trash")
+    (mu4e-refile-folder     . "/gmail/All Mail")
+    (smtpmail-smtp-user     . "moatcozza@gmail.com")
+    (mu4e-compose-signature . "---\nJimmy Cozza"))
+  t)
 
-(use-package exwm
-  :config
-  ;; Set the default number of workspaces
-  (setq exwm-workspace-number 5)
+;; if "gmail" is missing from the address or maildir, the account must be listed here
+(setq +mu4e-gmail-accounts '(("moatcozza@gmail.com" . "/moatcozza")))
 
-  ;; When window "class" updates, use it to set the buffer name
-  ;; (add-hook 'exwm-update-class-hook #'efs/exwm-update-class)
-
-  ;; These keys should always pass through to Emacs
-  (setq exwm-input-prefix-keys
-    '(?\C-x
-      ?\C-u
-      ?\C-h
-      ?\M-x
-      ?\M-`
-      ?\M-&
-      ?\M-:
-      ?\C-\M-j  ;; Buffer list
-      ?\C-\ ))  ;; Ctrl+Space
-
-  ;; Ctrl+Q will enable the next key to be sent directly
-  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
-
-  ;; Set up global key bindings.  These always work, no matter the input state!
-  ;; Keep in mind that changing this list after EXWM initializes has no effect.
-  (setq exwm-input-global-keys
-        `(
-          ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
-          ([?\s-r] . exwm-reset)
-
-          ;; Move between windows
-          ([s-left] . windmove-left)
-          ([s-right] . windmove-right)
-          ([s-up] . windmove-up)
-          ([s-down] . windmove-down)
-
-          ;; Launch applications via shell command
-          ([?\s-&] . (lambda (command)
-                       (interactive (list (read-shell-command "$ ")))
-                       (start-process-shell-command command nil command)))
-
-          ;; Switch workspace
-          ([?\s-w] . exwm-workspace-switch)
-
-          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
-          ,@(mapcar (lambda (i)
-                      `(,(kbd (format "s-%d" i)) .
-                        (lambda ()
-                          (interactive)
-                          (exwm-workspace-switch-create ,i))))
-                    (number-sequence 0 9))))
-
-  (exwm-enable))
+;; don't need to run cleanup after indexing for gmail
+(setq mu4e-index-cleanup nil
+      ;; because gmail uses labels as folders we can use lazy check since
+      ;; messages don't really "move"
+      mu4e-index-lazy-check t)
