4f0a23af55b1e784533818ecfb9faeea0127030f
Jimmy Cozza <moatcozza@gmail.com>
2021-12-27 09:38:22 -0700

    use autostart

--- awesome/configuration/window.lua
+++ awesome/configuration/window.lua
@@ -53,9 +53,9 @@ client.connect_signal("request::manage", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
+--client.connect_signal("mouse::enter", function(c)
+    --c:emit_signal("request::activate", "mouse_enter", {raise = false})
+--end)
 
 client.connect_signal("focus",
                       function(c) c.border_color = beautiful.border_focus end)
