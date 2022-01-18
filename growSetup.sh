884e103ab9dfcbcd1ba1a3a147609f39a318ec52
Jimmy Cozza <moatcozza@gmail.com>
2022-01-18 15:03:10 -0700

    eskitit

--- growSetup.sh
+++ growSetup.sh
@@ -30,34 +30,56 @@ yarn config set registry https://registry.npmjs.org
 ################ Setup app #######################
 ##################################################
 
-# cd $HOME/work/grow-job-processor
-# yarn link @grow/core
-echo "yarn install & link grow core"
+echo "Setup core"
 fnm use $CORE/.nvmrc
 yarn --cwd $CORE install
 yarn --cwd $CORE link
 
-# echo "yarn install & link grow-config"
-# fnm use $CONFIG/.nvmrc
-# yarn --cwd $CONFIG install
+echo "Setup config"
+fnm use $CONFIG/.nvmrc
+yarn --cwd $CONFIG install
 # yarn --cwd $CONFIG link
 
-echo "yarn install & link grow-app"
+echo "Setup app"
 fnm use $APP/.nvmrc
 yarn --cwd $APP install
-yarn --cwd $APP link @grow/core
+# yarn --cwd $APP link @grow/core
+cp $APP/docker-compose.yml.sample $APP/docker-compose.yml
+cp $APP/src/server/ssl/local.gogrow.com.crt /etc/ssl/certs/
+cp $APP/src/server/ssl/local.gogrow.com.key /etc/ssl/private/
 
-# cd $HOME/work/grow-app
-# cp docker-compose.yml.sample docker-compose.yml
-# yarn link @grow/core
-# yarn
+echo "Setup job-processor"
+fnm use $GJP/.nvmrc
+yarn --cwd $GJP install
+# yarn --cwd $GJP link @grow/core
 
-# cp src/server/ssl/local.gogrow.com.crt /etc/ssl/certs/
-# cp src/server/ssl/local.gogrow.com.key /etc/ssl/private/
+echo "Setup grow-aut5h"
+fnm use $AUTH/.nvmrc
+yarn --cwd $AUTH install
+cp $AUTH/.env.sample $AUTH/.env
 
 
-# aws configure
-# echo 'export PATH="/usr/local/opt/awscli@1/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
-# aws s3 cp s3://grow-files/nginx-local/nginx.conf /etc/nginx/
-# aws s3 cp s3://grow-files/nginx-local/local.grow.com.conf /etc/nginx/servers/
-# sed -i "" "s/ADDYOURUSERNAME/jimmy/g" /etc/nginx/servers/local.grow.com.conf
+
+aws configure
+echo 'export PATH="/usr/local/opt/awscli@1/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
+aws s3 cp s3://grow-files/nginx-local/nginx.conf /etc/nginx/
+aws s3 cp s3://grow-files/nginx-local/local.grow.com.conf /etc/nginx/servers/
+sed -i "" "s/$USER/jimmy/g" /etc/nginx/servers/local.grow.com.conf
+echo "Test to make sure your nginx conf is good.  If this isn't successful, figure it out"
+sudo nginx -t
+sudo nginx -s reload
+
+
+#Hosts File
+echo "127.0.0.1    localhost" >> /etc/hosts
+echo "255.255.255.255 broadcasthost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.0.1       local.gogrow.com" >> /etc/hosts
+echo "127.0.0.1       local.admin.gogrow.com" >> /etc/hosts
+echo "127.0.0.1:8080  local.admin.gogrow.com" >> /etc/hosts
+echo "127.0.0.1       grow-php" >> /etc/hosts
+echo "127.0.0.1       redis" >> /etc/hosts
+echo "127.0.0.1       redis-auth" >> /etc/hosts
+echo "127.0.0.1       redis-cache" >> /etc/hosts
+echo "127.0.0.1       postgres" >> /etc/hosts
+echo "127.0.0.1       rabbitmq" >> /etc/hosts
