691e9f735b4590cab223ebe0ec96bdbe0bc0652a
Jimmy Cozza <moatcozza@gmail.com>
2022-01-18 18:05:14 -0700

    this pretty much makes it rain on arch

--- growSetup.sh
+++ growSetup.sh
@@ -11,10 +11,12 @@ AUTH="$HOME/work/grow-auth"
 CONFIG="$HOME/work/grow-config"
 CORE="$HOME/work/core"
 GJP="$HOME/work/grow-job-processor"
+HOSTS="/etc/hosts"
+GVARS="$HOME/.growvars"
 
 echo "Creating growvars file & sourcing from .zshrc"
-touch $HOME/.growvars
-echo "source $CONFIG_FILES_PATH/growvars" >> $HOME/.zshrc
+touch $GVARS
+echo "source $GVARS" >> $HOME/.zshrc
 
 echo "creating work path and cloning grow repos"
 mkdir -p $HOME/work
@@ -26,21 +28,21 @@ git clone git@github.com:Growmies/core.git $CORE
 git clone git@github.com:Growmies/grow-job-processor.git $GJP
 
 
-##################################################
-################ Setup Node ######################
-##################################################
+#################################################
+############### Setup Node ######################
+#################################################
 echo "Enter your Grow NPM Token (npm.js -> profile pic -> Access Tokens -> Generate New): "
 read NPM_TOKEN
-echo export GROW_NPM_TOKEN=$NPM_TOKEN >> $HOME/.growvars && source $HOME/.zshrc
+echo export GROW_NPM_TOKEN=$NPM_TOKEN >> $GVARS && source $HOME/.zshrc
 echo "Logging into NPM.  Get your password handy."
 npm login --scope=@grow
 npm -g install yarn gulp
 yarn config set registry https://registry.npmjs.org
 
 
-##################################################
-################ Setup app #######################
-##################################################
+#################################################
+############### Setup app #######################
+#################################################
 
 echo "Setup core"
 fnm use $CORE/.nvmrc
@@ -57,8 +59,8 @@ fnm use $APP/.nvmrc
 yarn --cwd $APP install
 # yarn --cwd $APP link @grow/core
 cp $APP/docker-compose.yml.sample $APP/docker-compose.yml
-cp $APP/src/server/ssl/local.gogrow.com.crt /etc/ssl/certs/
-cp $APP/src/server/ssl/local.gogrow.com.key /etc/ssl/private/
+sudo cp $APP/src/server/ssl/local.gogrow.com.crt /etc/ssl/certs/
+sudo cp $APP/src/server/ssl/local.gogrow.com.key /etc/ssl/private/
 
 echo "Setup job-processor"
 fnm use $GJP/.nvmrc
@@ -76,25 +78,25 @@ aws configure
 echo 'export PATH="/usr/local/opt/awscli@1/bin:$PATH"' >> $HOME/.zshrc && source $HOME/.zshrc
 sudo aws s3 cp s3://grow-files/nginx-local/nginx.conf /etc/nginx/
 sudo aws s3 cp s3://grow-files/nginx-local/local.grow.com.conf /etc/nginx/servers/
-sed -i "" "s/ADDYOURUSERNAME/<YOURHOMEDIRECTORY>/g" /usr/local/etc/nginx/servers/local.grow.com.conf
+sed -i "" "s/ADDYOURUSERNAME/$USER/g" /usr/local/etc/nginx/servers/local.grow.com.conf
 echo "Test to make sure your nginx conf is good.  If this isn't successful, figure it out in the grow-app README.md"
 sudo nginx -t
 sudo nginx -s reload
 
 
 #Hosts File
-echo "127.0.0.1    localhost" | sudo tee -a /etc/hosts
-echo "255.255.255.255 broadcasthost" | sudo tee -a /etc/hosts
-echo "::1             localhost" | sudo tee -a /etc/hosts
-echo "127.0.0.1       local.gogrow.com" | sudo tee -a /etc/hosts
-echo "127.0.0.1       local.admin.gogrow.com" | sudo tee -a /etc/hosts
-echo "127.0.0.1:8080  local.admin.gogrow.com" | sudo tee -a /etc/hosts
-echo "127.0.0.1       grow-php" | sudo tee -a /etc/hosts
-echo "127.0.0.1       redis" | sudo tee -a /etc/hosts
-echo "127.0.0.1       redis-auth" | sudo tee -a /etc/hosts
-echo "127.0.0.1       redis-cache" | sudo tee -a /etc/hosts
-echo "127.0.0.1       postgres" | sudo tee -a /etc/hosts
-echo "127.0.0.1       rabbitmq" | sudo tee -a /etc/hosts
+echo "127.0.0.1    localhost" | sudo tee -a $HOSTS
+echo "255.255.255.255 broadcasthost" | sudo tee -a $HOSTS
+echo "::1             localhost" | sudo tee -a $HOSTS
+echo "127.0.0.1       local.gogrow.com" | sudo tee -a $HOSTS
+echo "127.0.0.1       local.admin.gogrow.com" | sudo tee -a $HOSTS
+echo "127.0.0.1:8080  local.admin.gogrow.com" | sudo tee -a $HOSTS
+echo "127.0.0.1       grow-php" | sudo tee -a $HOSTS
+echo "127.0.0.1       redis" | sudo tee -a $HOSTS
+echo "127.0.0.1       redis-auth" | sudo tee -a $HOSTS
+echo "127.0.0.1       redis-cache" | sudo tee -a $HOSTS
+echo "127.0.0.1       postgres" | sudo tee -a $HOSTS
+echo "127.0.0.1       rabbitmq" | sudo tee -a $HOSTS
 
 #growvars
 echo "Your Dev DB_USER:"
@@ -114,24 +116,24 @@ read NEW_RELIC_KEY
 echo "Grow cryptokey (Ask a dev for this):"
 read CRYPTO_KEY
 
-echo "export DEVELOPMENT_DB_HOST='appdb-dev.gogrow.com'" >> $HOME/.growvars
-echo "export DEVELOPMENT_DB_USER='$DEV_DB_USER'" >> $HOME/.growvars
-echo "export DEVELOPMENT_DB_DB='grow'" >> $HOME/.growvars
-echo "export DEVELOPMENT_DB_PASSWORD='$DEV_DB_PASS'" >> $HOME/.growvars
-echo "export STAGING_DB_HOST='appdb-stg.gogrow.com'" >> $HOME/.growvars
-echo "export STAGING_DB_USER='$STAGING_DB_USER'" >> $HOME/.growvars
-echo "export STAGING_DB_DB='grow'" >> $HOME/.growvars
-echo "export STAGING_DB_PASSWORD='$STAGING_DB_PASS'" >> $HOME/.growvars
-echo "export PRODUCTION_DB_HOST='appdb-prod-source.gogrow.com'" >> $HOME/.growvars
-echo "export PRODUCTION_DB_USER='$PROD_DB_USER'" >> $HOME/.growvars
-echo "export PRODUCTION_DB_DB='grow'" >> $HOME/.growvars
-echo "export PRODUCTION_DB_PASSWORD='$PROD_DB_PASS'" >> $HOME/.growvars
-echo "export REDIS_APP_CACHE_HOST_MASTER='redis-cache'" >> $HOME/.growvars
-echo "export REDIS_APP_CACHE_HOST_REPLICA='redis-cache'" >> $HOME/.growvars
-echo "export REDIS_APP_CACHE_PORT='6381'" >> $HOME/.growvars
-echo "export REDIS_APP_CACHE_TTL='86400' # 1 day" >> $HOME/.growvars
-echo "export NEW_RELIC_LICENSE_KEY='$NEW_RELIC_KEY'" >> $HOME/.growvars
-echo "export COMMON_GROW_APP_CRYPTOKEY='$CRYPTO_KEY'" >> $HOME/.growvars
+echo "export DEVELOPMENT_DB_HOST='appdb-dev.gogrow.com'" >> $GVARS
+echo "export DEVELOPMENT_DB_USER='$DEV_DB_USER'" >> $GVARS
+echo "export DEVELOPMENT_DB_DB='grow'" >> $GVARS
+echo "export DEVELOPMENT_DB_PASSWORD='$DEV_DB_PASS'" >> $GVARS
+echo "export STAGING_DB_HOST='appdb-stg.gogrow.com'" >> $GVARS
+echo "export STAGING_DB_USER='$STAGING_DB_USER'" >> $GVARS
+echo "export STAGING_DB_DB='grow'" >> $GVARS
+echo "export STAGING_DB_PASSWORD='$STAGING_DB_PASS'" >> $GVARS
+echo "export PRODUCTION_DB_HOST='appdb-prod-source.gogrow.com'" >> $GVARS
+echo "export PRODUCTION_DB_USER='$PROD_DB_USER'" >> $GVARS
+echo "export PRODUCTION_DB_DB='grow'" >> $GVARS
+echo "export PRODUCTION_DB_PASSWORD='$PROD_DB_PASS'" >> $GVARS
+echo "export REDIS_APP_CACHE_HOST_MASTER='redis-cache'" >> $GVARS
+echo "export REDIS_APP_CACHE_HOST_REPLICA='redis-cache'" >> $GVARS
+echo "export REDIS_APP_CACHE_PORT='6381'" >> $GVARS
+echo "export REDIS_APP_CACHE_TTL='86400' # 1 day" >> $GVARS
+echo "export NEW_RELIC_LICENSE_KEY='$NEW_RELIC_KEY'" >> $GVARS
+echo "export COMMON_GROW_APP_CRYPTOKEY='$CRYPTO_KEY'" >> $GVARS
 
 source $HOME/.zshrc
 
@@ -139,6 +141,8 @@ echo "Are you connected to the VPN? [ y/n ]"
 read vpn
 if [[ $vpn = "y" ]]
 then
+    echo "Spin up the docker compose"
+    yarn --cwd $APP docker
     echo "Setup local db"
     echo "migrate"
     gulp --gulpfile $APP/gulpfile.js migrate
