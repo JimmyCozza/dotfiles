b086a0971b0c34badb4dfba60e63977556095788
Jimmy Cozza <moatcozza@gmail.com>
2022-01-24 14:02:53 -0700

    replace the nerd-fonts with nerd-fonts-complete

--- archSetup.sh
+++ archSetup.sh
@@ -12,7 +12,7 @@ mkdir $HOME/tools
 
 ARCH_LIST="alacritty fzf ripgrep tmux firefox bat discord docker docker-compose rofi sxhkd flameshot aws-cli nginx python2 base-devel cmake unzip ninja tree-sitter curl zsh python-pip ruby lazygit"
 
-AUR_LIST="fnm-bin lazydocker nerd-fonts-go-mono nerd-fonts-fira-code zsh-syntax-highlighting-git slack-desktop tableplus tdrop-git awesome-git emacs-git google-chrome-dev playerctl arcolinux-logout"
+AUR_LIST="fnm-bin lazydocker nerd-fonts-complete zsh-syntax-highlighting-git slack-desktop tableplus tdrop-git awesome-git emacs-git google-chrome-dev playerctl arcolinux-logout"
 
 echo "Fetching standard arch packages"
 sudo pacman -Syu --noconfirm $ARCH_LIST
