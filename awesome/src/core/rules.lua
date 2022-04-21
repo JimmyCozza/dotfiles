bde091de4b0ba9d22f7297992823a471855796a9
Jimmy Cozza <moatcozza@gmail.com>
2022-04-21 16:37:07 -0600

    stylua

--- awesome/src/core/rules.lua
+++ awesome/src/core/rules.lua
@@ -4,49 +4,48 @@
 
 -- Awesome Libs
 local awful = require "awful"
+local ruled = require "ruled"
 local beautiful = require "beautiful"
 
-awful.rules.rules = {
-  {
-    rule = {},
-    properties = {
-      border_width = beautiful.border_width,
-      border_color = beautiful.border_normal,
-      focus = awful.client.focus.filter,
-      raise = true,
-      keys = require "../../mappings/client_keys",
-      buttons = require "../../mappings/client_buttons",
-      screen = awful.screen.preferred,
-      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+ruled.client.append_rule {
+  rule = {},
+  properties = {
+    border_width = beautiful.border_width,
+    border_color = beautiful.border_normal,
+    focus = awful.client.focus.filter,
+    raise = true,
+    keys = require "../../mappings/client_keys",
+    buttons = require "../../mappings/client_buttons",
+    screen = awful.screen.preferred,
+    placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+  },
+}
+-- Clients should always start as slave instead of master
+ruled.client.append_rule { rule = {}, properties = {}, callback = awful.client.setslave }
+ruled.client.append_rule {
+  rule_any = {
+    instance = {},
+    class = {
+      "Arandr",
+      "Lxappearance",
+      "kdeconnect.app",
+      "zoom",
+      "file-roller",
+      "File-roller",
+    },
+    name = {},
+    role = {
+      "AlarmWindow",
+      "ConfigManager",
+      "pop-up",
     },
   },
-  -- Clients should always start as slave instead of master
-  { rule = {}, properties = {}, callback = awful.client.setslave },
-  {
-    rule_any = {
-      instance = {},
-      class = {
-        "Arandr",
-        "Lxappearance",
-        "kdeconnect.app",
-        "zoom",
-        "file-roller",
-        "File-roller",
-      },
-      name = {},
-      role = {
-        "AlarmWindow",
-        "ConfigManager",
-        "pop-up",
-      },
-    },
-    properties = { floating = true, titlebars_enabled = true },
+  properties = { floating = true, titlebars_enabled = true },
+}
+ruled.client.append_rule {
+  id = "titlebar",
+  rule_any = {
+    type = { "normal", "dialog", "modal", "utility" },
   },
-  {
-    id = "titlebar",
-    rule_any = {
-      type = { "normal", "dialog", "modal", "utility" },
-    },
-    properties = { titlebars_enabled = true },
-  },
+  properties = { titlebars_enabled = true },
 }
