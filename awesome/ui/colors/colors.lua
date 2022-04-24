5ed361b009d55796124b692ab3fafab543be5b6f
Jimmy Cozza <moatcozza@gmail.com>
2022-04-24 10:20:48 -0600

    add the layout I think I want

---
+++ awesome/ui/colors/colors.lua
@@ -0,0 +1,9 @@
+local gears = require"gears"
+
+local base_colors = require "src.theme.crylia"
+local user_colors = require("src.theme."..user_vars.colorscheme)
+
+return gears.table.join(
+    base_colors,
+    user_colors
+)
