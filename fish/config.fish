c25c223bf9f5dd49ddc1cc52adb06610c9f99654
Jimmy Cozza <moatcozza@gmail.com>
2022-06-30 09:37:27 -0600

    fishy fishy

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 export CONFIG_FILES_PATH=$HOME/dotfiles
 
+set -gx PATH $PATH ~/go/bin/
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 source $CONFIG_FILES_PATH/aliases
+direnv hook fish | source
