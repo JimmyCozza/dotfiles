89647482b6a1fcc2a36352b6ca61bd4a69dd5add
Jimmy Cozza <moatcozza@gmail.com>
2022-07-22 19:22:35 -0600

    meow

--- doom/custom.el
+++ doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("025acf94eb56f1b569f853f77effb1aa5006947a9df3a913f52fcb8daf9a15b2" default))
  '(elfeed-feeds
    '("https://news.ycombinator.com/rss" "https://sachachua.com/blog/feed/")))
 (custom-set-faces
