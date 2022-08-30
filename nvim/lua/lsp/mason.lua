f6fa32e780280a7b6c50c48b23daefa8782f9b97
Jimmy Cozza <moatcozza@gmail.com>
2022-08-29 22:35:35 -0600

    freaky styley

--- nvim/lua/lsp/mason.lua
+++ nvim/lua/lsp/mason.lua
@@ -3,20 +3,20 @@ if not ok then
   return
 end
 
-mason.setup({
-    ui = {
-        icons = {
-          package_installed = "",
-          package_pending = "",
-          package_uninstalled = "ﮊ",
-        }
-    }
-})
+mason.setup {
+  ui = {
+    icons = {
+      package_installed = "",
+      package_pending = "",
+      package_uninstalled = "ﮊ",
+    },
+  },
+}
 
 local ok, mason_lsp_config = pcall(require, "mason-lspconfig")
 if not ok then
   return
 end
-mason_lsp_config.setup({
-    ensure_installed = { "sumneko_lua", "gopls" }
-})
+mason_lsp_config.setup {
+  ensure_installed = { "sumneko_lua", "gopls" },
+}
