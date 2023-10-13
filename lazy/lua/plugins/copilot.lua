3739d38644f6fe5b2174727f6b8a7b40eb617aed
Jimmy Cozza <moatcozza@gmail.com>
2023-10-13 14:08:30 -0600

    lazy?

---
+++ lazy/lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  -- { "github/copilot.vim" },
+  {
+    "zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()
+      end, 100)
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
