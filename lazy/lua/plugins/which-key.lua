f030664cd2a852a7929e20886ed252f448da1e58
Jimmy Cozza <moatcozza@gmail.com>
2023-10-15 21:38:40 -0600

    eh...not so sure about this

--- lazy/lua/plugins/which-key.lua
+++ lazy/lua/plugins/which-key.lua
@@ -1,21 +1,18 @@
-local opts = {
-  plugins = {
-    presets = {
-      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-    },
-  },
-  window = {
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 90,
-  },
-}
+local opts = {}
 
 return {
   {
     "folke/which-key.nvim",
-    config = function()
-      local wk = require("which-key")
-      wk.setup(opts)
-    end,
+    opts = {
+      plugins = {
+        presets = {
+          operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+        },
+      },
+      window = {
+        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        winblend = 90,
+      },
+    },
   },
 }
