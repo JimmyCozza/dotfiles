d90008d658e4530472876f50d2275b12e663ecbb
Jimmy Cozza <moatcozza@gmail.com>
2024-01-26 06:01:05 -0700

    gippity

--- newvim/lua/plugins/completion.lua
+++ newvim/lua/plugins/completion.lua
@@ -15,6 +15,19 @@ return {
   },
   { "zbirenbaum/copilot-cmp" },
   {
+    "jackMort/ChatGPT.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function() 
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://Personal/OpenAI/credential --no-newline",
+      })
+    end,
+  },
+  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
