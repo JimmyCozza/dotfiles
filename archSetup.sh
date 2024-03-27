8b61423d1a272f8143d4898c13efdbe0df30d8e7
Jimmy Cozza <moatcozza@gmail.com>
2024-03-27 14:19:06 -0600

    finish cleaning up

--- archSetup.sh
+++ archSetup.sh
@@ -12,7 +12,7 @@ cd $HOME/tools/yay && makepkg -si
 
 ARCH_LIST="ripgrep docker docker-compose cmake unzip ninja tree-sitter curl zsh python-pip ruby lazygit picom direnv wezterm pipewire pipewire-audio brightnessctl thunar thunar-volman gvfs libinput wayland wlroots libxkbcommon wayland-protocols pkgconf bemenu bemenu-wayland firefox"
 
-AUR_LIST="fnm-bin lazydocker ttf-jetbrains-mono-nerd zsh-syntax-highlighting-git slack-desktop beekeeper-studio-appimage feh wl-clipboard python2"
+AUR_LIST="fnm-bin lazydocker ttf-jetbrains-mono-nerd zsh-syntax-highlighting-git slack-desktop beekeeper-studio-appimage feh wl-clipboard python2 python-pynvim"
 
 echo "Fetching standard arch packages"
 sudo pacman -Syu --noconfirm $ARCH_LIST
