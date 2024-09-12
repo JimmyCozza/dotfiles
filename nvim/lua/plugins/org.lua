044d9d6193a30ee18c8df7559a346c8a41077e35
Jimmy Cozza <moatcozza@gmail.com>
2024-09-12 07:12:26 -0600

    markdown viewer

--- nvim/lua/plugins/org.lua
+++ nvim/lua/plugins/org.lua
@@ -1,21 +1,22 @@
-return{
-{
-  'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
-  ft = { 'org' },
-  config = function()
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/notes/org/**/*',
-      org_default_notes_file = '~/notes/org/notes.org',
-    })
+return {
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/notes/org/**/*',
+        org_default_notes_file = '~/notes/org/notes.org',
+      })
 
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    -- require('nvim-treesitter.configs').setup({
-    --   ensure_installed = 'all',
-    --   ignore_install = { 'org' },
-    -- })
-  end,
-}
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  }
+
 }
