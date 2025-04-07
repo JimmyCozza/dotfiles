f8e0b59119c91543253c77b0ca019f22e9f93ed9
Jimmy Cozza <moatcozza@gmail.com>
2025-04-07 13:14:15 -0600

    remove mcp

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -81,28 +81,6 @@ return {
     },
   },
   {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    build = "npm install -g mcp-hub@latest",
-    config = function()
-      require("mcphub").setup({
-        port = 5555,
-        config = vim.fn.expand("~/.config/Claude/claude_desktop_config.json"),
-
-        on_ready = function(hub) end,
-        on_error = function(err) end,
-        log = {
-          level = vim.log.levels.WARN,
-          to_file = false,
-          file_path = nil,
-          prefix = "MCPHub",
-        },
-      })
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
