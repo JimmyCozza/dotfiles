be01228a23c463c0f76970a4f70c49e66e64c95c
Jimmy Cozza <moatcozza@gmail.com>
2025-08-27 15:06:20 -0600

    close enough

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,12 +1,24 @@
 return {
+  -- {
+  --   "ribru17/bamboo.nvim",
+  --   config = function()
+  --     require("bamboo").setup({
+  --       style = "multiplex",
+  --       dim_inactive = true,
+  --     })
+  --     require("bamboo").load()
+  --   end,
+  -- },
   {
-    "ribru17/bamboo.nvim",
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
     config = function()
-      require("bamboo").setup({
-        style = "multiplex",
-        dim_inactive = true,
+      require("everforest").setup({
+        require("everforest").load()
       })
-      require("bamboo").load()
     end,
-  },
+  }
 }
